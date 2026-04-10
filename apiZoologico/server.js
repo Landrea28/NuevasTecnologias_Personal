33cefb3fb5a814047f2390e019aff8ce3af1d3a6
Lina Bello <bellolina011@gmail.com>
2026-04-09 23:11:47 -0500

    date:09/04/26

--- apiZoologico/server.js
+++ apiZoologico/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express"); 
 const mongoose = require("mongoose"); 
 const cors = require("cors"); 
+const authRoutes = require("./routers/authentication");
  
 const app = express(); 
  
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
  
 // Rutas 
 app.use("/api/animals", require("./routes/animalRoutes")); 
+app.use("/api", authRoutes)
  
 // Ruta base 
 app.get("/", (req, res) => { 
